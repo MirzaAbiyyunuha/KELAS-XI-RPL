acb85b6e5fd1e8080d0e8f9eed6c754a108685a2
MirzaAbiyyunuha <mirzaabiyunuha0311@gmail.com>
2026-08-11 15:41:02 +0700

    Add files via upload

---
+++ semester 1/tugas-pak-isa/video/windows form/Admin form/Contoh soal part 1/Contoh soal part 1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contoh_soal_part_1
+{
+    public partial class Form1Login : Form
+    {
+        public Form1Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3Dasboard dasboard = new Form3Dasboard();
+            this.Hide();
+            dasboard .Show();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Form2register register = new Form2register();
+            this.Hide();
+            register .Show();
+        }
+    }
+}
---
+++ semester 1/tugas-pak-isa/video/windows form/Admin form/Contoh soal part 1/Contoh soal part 1/Form3Dasboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contoh_soal_part_1
+{
+    public partial class Form3Dasboard : Form
+    {
+        public Form3Dasboard()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
