8e628db9fc5c5e2bfce56e86ca0d638f69a2cb2e
MirzaAbiyyunuha <mirzaabiyunuha0311@gmail.com>
2026-08-12 20:56:21 +0700

    Add files via upload

---
+++ semester 1/tugas-pak-isa/kelas/12-8-2026 ,  tugas tabungan/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnTarik = new System.Windows.Forms.Button();
+            this.btnMenabung = new System.Windows.Forms.Button();
+            this.cmbKelas = new System.Windows.Forms.ComboBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtNominal = new System.Windows.Forms.TextBox();
+            this.txtSaldo = new System.Windows.Forms.TextBox();
+            this.txtNIS = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dgvTabungan = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabungan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(638, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(189, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tabungan siswa";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnTarik);
+            this.panel2.Controls.Add(this.btnMenabung);
+            this.panel2.Controls.Add(this.cmbKelas);
+            this.panel2.Controls.Add(this.txtNama);
+            this.panel2.Controls.Add(this.txtNominal);
+            this.panel2.Controls.Add(this.txtSaldo);
+            this.panel2.Controls.Add(this.txtNIS);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(3, 102);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(638, 298);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // btnTarik
+            // 
+            this.btnTarik.Location = new System.Drawing.Point(257, 220);
+            this.btnTarik.Name = "btnTarik";
+            this.btnTarik.Size = new System.Drawing.Size(75, 23);
+            this.btnTarik.TabIndex = 3;
+            this.btnTarik.Text = "Tarik";
+            this.btnTarik.UseVisualStyleBackColor = true;
+            this.btnTarik.Click += new System.EventHandler(this.btnTarik_Click);
+            // 
+            // btnMenabung
+            // 
+            this.btnMenabung.Location = new System.Drawing.Point(176, 220);
+            this.btnMenabung.Name = "btnMenabung";
+            this.btnMenabung.Size = new System.Drawing.Size(75, 23);
+            this.btnMenabung.TabIndex = 3;
+            this.btnMenabung.Text = "Menabung";
+            this.btnMenabung.UseVisualStyleBackColor = true;
+            this.btnMenabung.Click += new System.EventHandler(this.btnMenabung_Click);
+            // 
+            // cmbKelas
+            // 
+            this.cmbKelas.FormattingEnabled = true;
+            this.cmbKelas.Items.AddRange(new object[] {
+            "X TKJ",
+            "XI TKJ",
+            "XII TKJ",
+            "X RPL",
+            "XI RPL",
+            "XII RPL",
+            "X DKV",
+            "XI DKV",
+            "XII DKV"});
+            this.cmbKelas.Location = new System.Drawing.Point(145, 82);
+            this.cmbKelas.Name = "cmbKelas";
+            this.cmbKelas.Size = new System.Drawing.Size(183, 21);
+            this.cmbKelas.TabIndex = 2;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(145, 46);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(187, 20);
+            this.txtNama.TabIndex = 1;
+            // 
+            // txtNominal
+            // 
+            this.txtNominal.Location = new System.Drawing.Point(145, 132);
+            this.txtNominal.Name = "txtNominal";
+            this.txtNominal.Size = new System.Drawing.Size(183, 20);
+            this.txtNominal.TabIndex = 1;
+            // 
+            // txtSaldo
+            // 
+            this.txtSaldo.Location = new System.Drawing.Point(147, 172);
+            this.txtSaldo.Name = "txtSaldo";
+            this.txtSaldo.ReadOnly = true;
+            this.txtSaldo.Size = new System.Drawing.Size(183, 20);
+            this.txtSaldo.TabIndex = 1;
+            // 
+            // txtNIS
+            // 
+            this.txtNIS.Location = new System.Drawing.Point(145, 6);
+            this.txtNIS.Name = "txtNIS";
+            this.txtNIS.Size = new System.Drawing.Size(183, 20);
+            this.txtNIS.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Kelas  Siswa :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(5, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nama  Siswa :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(115, 172);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 17);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Rp";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(115, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Rp";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(9, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 17);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Nominal :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Saldo :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "NIS Siswa :";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.dgvTabungan);
+            this.panel3.Location = new System.Drawing.Point(2, 429);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(639, 240);
+            this.panel3.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(2, 5);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 20);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Data tabungan ";
+            // 
+            // dgvTabungan
+            // 
+            this.dgvTabungan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTabungan.Location = new System.Drawing.Point(0, 34);
+            this.dgvTabungan.Name = "dgvTabungan";
+            this.dgvTabungan.Size = new System.Drawing.Size(636, 203);
+            this.dgvTabungan.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 666);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabungan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbKelas;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtNIS;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnTarik;
+        private System.Windows.Forms.Button btnMenabung;
+        private System.Windows.Forms.TextBox txtSaldo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dgvTabungan;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNominal;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        
+    }
+}
+
